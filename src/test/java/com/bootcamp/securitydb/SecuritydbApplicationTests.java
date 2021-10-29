c401cd09d734bda808d1ab38a52a71adf4e10bbd
Agsvit <goncalovit@gmail.com>
2021-10-29 14:18:22 +0100

    Initial commit

---
+++ src/test/java/com/bootcamp/securitydb/SecuritydbApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bootcamp.securitydb;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritydbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
